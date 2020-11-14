cd4db74204ca6a926244a71f08de85c6ea627055
unknown <richardcarmenscott@gmail.com>
2020-11-14 16:12:54 -0500

    add env variables

--- src/config.js
+++ src/config.js
@@ -5,4 +5,6 @@ module.exports = {
   TEST_DB_URL: process.env.TEST_DB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
+  MIGRATION_DB_USER: process.env.MIGRATION_DB_USER,
+  MIGRATION_DB_PASS: process.env.MIGRATION_DB_PASS,
 };
