afb01eefbe5cffa69e06924f3b213a2adbffdc6b
unknown <richardcarmenscott@gmail.com>
2020-11-14 17:17:53 -0500

    update

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
-  host: '127.0.0.1',
-  port: 5432,
   database: 'spaced-repetition',
   username: 'user',
   password: 'pass',
