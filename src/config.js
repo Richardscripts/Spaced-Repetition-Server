e1b435c362280072234b297be5b6c65507d1595b
unknown <richardcarmenscott@gmail.com>
2020-11-14 16:18:11 -0500

    updated env

--- src/config.js
+++ src/config.js
@@ -5,4 +5,5 @@ module.exports = {
   TEST_DB_URL: process.env.TEST_DB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
+  MIGRATION_DB_PASS: process.env.MIGRATION_DB_PASS,
 };
