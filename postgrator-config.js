2d55ce5eafffb0e8bfa5674da3f465a54f1cf44a
unknown <richardcarmenscott@gmail.com>
2020-11-14 17:16:44 -0500

    updated env

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
-  connectionString:
-    process.env.NODE_ENV === 'test'
-      ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL,
+  host: '127.0.0.1',
+  port: 5432,
+  database: 'spaced-repetition',
+  username: 'user',
+  password: 'pass',
 };
