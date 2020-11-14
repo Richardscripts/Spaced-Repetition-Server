3ac91664aa99ffdeff9a520f94c0d70ebde0f073
unknown <richardcarmenscott@gmail.com>
2020-11-14 16:35:05 -0500

    env variable

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 module.exports = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
-  DATABASE_URL: process.env.DB_URL,
-  MIGRATION_DATABASE_PASS: process.env.MIGRATION_DB_PASS,
-  TEST_DB_URL: process.env.TEST_DB_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DB_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
-  MIGRATION_DB_PASS: process.env.MIGRATION_DB_PASS,
 };
